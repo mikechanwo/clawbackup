e3976f3cbb33164e358fd65646d270c136b0dc34
mikechanwo <mikechanwo@gmail.com>
2026-03-26 14:38:00 +0800

    Add contracts

---
+++ contracts.py
@@ -0,0 +1,18 @@
+# Polymarket Contract Addresses
+# Official: https://docs.pymarket.com/developers/resources/contract-addresses
+
+# CTF Exchange (CORRECT ADDRESS!)
+CTF_EXCHANGE = "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E"
+
+# USDC (Polygon)
+USDC_E = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+
+# Conditional Tokens
+CONDITIONAL_TOKENS = "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"
+
+# All addresses
+ALL_ADDRESSES = {
+    "ctf_exchange": CTF_EXCHANGE,
+    "usdc": USDC_E,
+    "conditional_tokens": CONDITIONAL_TOKENS
+}
